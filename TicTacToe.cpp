ace003dfa5375b60d87f276ee9ce782fb18ba0e1
alro9829 <alro9829@colorado.edu>
2022-02-10 23:50:48 -0700

    finished PlaceMarker function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -33,29 +34,6 @@ void DisplayBoard(vector<vector<int> > board) {
   cout << endl;
 }
 
-void PlaceMarker(int i, int marker) {
-
-   switch (i) {
-   case 1:
-      break;
-   case 2:
-      break;
-   case 3:
-      break;
-   case 4:
-      break;
-   case 5:
-      break;
-   case 6:
-      break;
-   case 7:
-      break;
-   case 8:
-      break;
-   case 9:
-      break;
-
-   }
 int GetPlayerChoice() {
   string choice = "";
   cout << "Please enter your choice: ";
@@ -63,7 +41,71 @@ int GetPlayerChoice() {
   return stoi(choice);
 }
 
+vector<vector<int>> PlaceMarker(int i, bool marker, vector<vector<int>> board) {
+   switch (i) {
+   case 1:
+      board[0][0] = marker + 1;
+      return board;
+      break;
+   case 2:
+      board[0][1] = marker + 1;
+      return board;
+      break;
+   case 3:
+      board[0][2] = marker + 1;
+      return board;
+      break;
+   case 4:
+      board[1][0] = marker + 1;
+      return board;
+      break;
+   case 5:
+      board[1][1] = marker + 1;
+      return board;
+      break;
+   case 6:
+      board[1][2] = marker + 1;
+      return board;
+      break;
+   case 7:
+      board[2][0] = marker + 1;
+      return board;
+      break;
+   case 8:
+      board[2][1] = marker + 1;
+      return board;
+      break;
+   case 9:
+      board[2][2] = marker + 1;
+      return board;
+      break;
+   default:
+      return board;
+   }
+}
+
+void Play(vector<vector<int>> board) {
+
+   DisplayBoard(board);
+
+   bool player = true;
+
+   for (int i = 0; i < 9; i++) {
+      board = PlaceMarker(GetPlayerChoice(), player, board);
+
+      if (player) {
+         player = false;
+      } else {
+         player = true;
+      }
+
+      DisplayBoard(board);
+   }
+}
+
+
+
 int main() {
-  vector<vector<int> > board = CreateBoard();
-  DisplayBoard(board);
+   vector<vector<int> > board = CreateBoard();
+   Play(board);
 }
