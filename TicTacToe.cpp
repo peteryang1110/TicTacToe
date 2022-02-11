679e943293f50381a2d21a07d2bccef1c342fe3c
Peter Yang <yuya0723@colorado.edu>
2022-02-10 20:20:52 -0700

    Implement function DisplayBoard

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -7,6 +7,33 @@ vector<vector<int> > CreateBoard() {
   return vector<vector<int> >(3, vector<int>(3, 0));
 }
 
+void DisplayBoard(vector<vector<int> > board) {
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      switch (board[i][j]) {
+        case 1:
+          cout << " O";
+          break;
+        case 2:
+          cout << " X";
+          break;
+        default:
+          cout << " " << i + j * 3 + 1;
+      }
+      if (j != 2) {
+        cout << " |";
+      }
+    }
+    if (i != 2) {
+      cout << endl
+           << "--- --- ---"
+           << endl;
+    }
+  }
+  cout << endl;
+}
+
 int main() {
   vector<vector<int> > board = CreateBoard();
+  DisplayBoard(board);
 }
